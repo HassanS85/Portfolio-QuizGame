1790ef2ddf53aa8d351e3055de87deabf7a75314
Hassan <hassan.seedat07@gmail.com>
2022-05-30 20:28:44 +0100

    added in new HTML

---
+++ quiz.js
@@ -0,0 +1,14 @@
+// select all elements
+
+const start = document.getElementById("start");
+
+const quiz = document.getElementById("quiz");
+
+const question = document.getElementById("question");
+
+const qImg = document.getElementById("qImg");
+
+
+
+
+
