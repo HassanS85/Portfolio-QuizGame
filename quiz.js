e7e04bd4f7a3e46bdf2d4a19f48feab5bc1e8989
Hassan <hassan.seedat07@gmail.com>
2022-06-01 14:18:54 +0100

    added in further pseudocode for JS functions

--- quiz.js
+++ quiz.js
@@ -29,21 +29,21 @@ const scoreDiv = document.getElementById("scoreContainer");
 let questions = [
     {
         question : "What does HTML stand for?",
-        imgSrc: "img/html.png",
+        imgSrc: ".png",
         choiceA: "Correct",
         choiceB: "Incorrect",
         choiceC: "Incorrect",
         correct: "A"
     }, {
         question : "What does CSS stand for?",
-        imgSrc: "img/html.png",
+        imgSrc: ".png",
         choiceA: "Incorrect",
         choiceB: "Correct",
         choiceC: "Incorrect",
         correct: "B"
     }, {
         question : "What does JS stand for?",
-        imgSrc: "img/html.png",
+        imgSrc: ".png",
         choiceA: "Incorrect",
         choiceB: "Incorrect",
         choiceC: "Correct",
@@ -56,6 +56,13 @@ const previousQuestion = questions.length - 1;
 // current question starts from 0
 let currentQuestion = 0;
 let count = 0;
+
+//creating the timer for the game
+const questionTime = 10; // 10s
+const timerWidth = 150; // 150px
+const timerUnit = gaugeWidth / questionTime;
+let score = 0;
+
 
 //asks a question
 //uses currentQuestion starting from 0
@@ -88,4 +95,43 @@ function quizProgress(){
 }
 
 start.addEventListener("click",startQuiz);
+
+// start quiz
+function startQuiz(){
+    
+    askQuestion();
+    
+    qusetionProgress();
+    questionCounter();
+    timesInterval = setInterval(questionCounter,1000); // 1000ms = 1s
+
+}
+
+// the progress so far.
+function renderProgress(){
+    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
+        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+    }
+}
+
+// counter function to show the progress
+
+
+// show the score of the quiz so far
+
+
+//function to see if the answer is correct or not
+
+//function to change the colour of the options(A,B,C) if the incorrect answer is given
+
+//function to change the colour of the options(A,B,C) if the correct answer is given
+
+
+//function to show the score currently in percentage.
+
+
+//calculate the amount answered correctly/incorrectly.
+//will need to use (Math.round here.)
+
+// 
 
